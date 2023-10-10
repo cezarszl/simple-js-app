a6712e3cdfc4dfd269e4bee981f8a0c56d44ec1d
cezarszl <cezarszlfedora@gmail.com>
2023-10-11 00:33:36 +0200

    Started scripts.js from scratch with an array of pokemons.
    Full-Stack Immersion Exercise 1.2

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,5 @@
-alert('Hello  world');
-favoriteFood = 'Julius Cesear';
-document.write(favoriteFood);
+let pokemonList = [
+    { name: "Bulbasaur", type: ["grass", "poison"], height: 0.7 },
+    { name: "Caterpie", type: "Bug", height: 0.3 },
+    { name: "Rattata", type: "Normal", height: 0.3 }
+];
