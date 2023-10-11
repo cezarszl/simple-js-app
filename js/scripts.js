ca568c1bfe39764bbbb550bf7fb0a741688b1e6e
cezarszl <cezarszlfedora@gmail.com>
2023-10-11 17:15:27 +0200

    Added a loop to print names and heights
    Full-Stack Immersion Exercise 1.3 Part1

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,9 @@
 let pokemonList = [
-    { name: "Bulbasaur", type: ["grass", "poison"], height: 0.7 },
+    { name: "Bulbasaur", type: ["Grass", "Poison"], height: 0.7 },
     { name: "Caterpie", type: "Bug", height: 0.3 },
     { name: "Rattata", type: "Normal", height: 0.3 }
 ];
+
+for (let i=0; i <= pokemonList.length; i++) {
+    document.write("<p>" + pokemonList[i].name + " (height: " + pokemonList[i].height + ") </p>" );
+}
