4099460c4e68b89d523411ed18fd490139d52f30
cezarszl <cezarszlfedora@gmail.com>
2023-10-18 01:39:14 +0200

    Added typeof and Object.keys conditions and new public function
    Full-Stack Immersion Exercise 1.5 Bonus Task

--- js/scripts.js
+++ js/scripts.js
@@ -7,21 +7,43 @@ let pokemonRepository = (function () {
     ];
 
     function add (pokemon) {
-      pokemonList.push(pokemon);
+      // Checking if modification is an object
+      if(typeof pokemon === "object") {
+        // Checking if the keys are the same 
+        if(Object.keys(pokemon).toString() === Object.keys(pokemonList[0]).toString()) {
+          pokemonList.push(pokemon);
+        }
+        else {
+          alert("Object must have following keys: name, type, height");
+        }
+      }
+      else {
+        alert("Added pokemon must be an object");
+      }
     }
-
+    // Function to find Pokemons info only by name
+    function getByName (pokemonName) {
+      let pokemon = pokemonList.filter(pokemon => pokemon.name === pokemonName);
+         return pokemon;
+    }
     function getAll () {
       return pokemonList;
     }
 
     return {
       add: add,
+      getByName: getByName,
       getAll: getAll
     };
 })();
 
 // Adding a pokemon 
 pokemonRepository.add({ name: 'Hypno', type: 'PSYCHIC', height: 1.6});
+// pokemonRepository.add("string");
+// pokemonRepository.add({name: 'Hypno', size: 0});
+
+// Printing info by name (an array)
+console.log(pokemonRepository.getByName("Bulbasaur"));
 
 // Setting up a table
 document.write("<table><caption>My Pokémons</caption><thead><tr><th>Name</th><th>Type</th><th>Height(m)</th></tr></thead><tbody>");
@@ -36,7 +58,7 @@ pokemonRepository.getAll().forEach(function (pokemon) {
 });
 
 //Closing table tags
-document.write("</tbody>></table>");
+document.write("</tbody></table>");
 
 // A do while loop checking checking which pokemon is higher than 1m and printing it
 let i = 0;
